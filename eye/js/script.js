502a5970c8ae71cff99e1a42ff57b1af8826146f
mohit838 <mohitul449@gmail.com>
2023-09-25 21:16:25 +0600

    Navbar create

--- eye/js/script.js
+++ eye/js/script.js
@@ -1,3 +1,10 @@
 window.addEventListener('load', function () {
   AOS.init();
 });
+
+const button = document.querySelector('#menu-button'); // Hamburger Icon
+const menu = document.querySelector('#menu'); // Menu
+
+button.addEventListener('click', () => {
+  menu.classList.toggle('hidden');
+});

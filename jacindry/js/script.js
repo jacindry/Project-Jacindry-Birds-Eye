d58371f964f741b4a2f358bb66e70bd63973cbb0
mohit838 <mohitul449@gmail.com>
2023-09-27 15:02:49 +0600

    working with slider

--- jacindry/js/script.js
+++ jacindry/js/script.js
@@ -1,3 +1,38 @@
 window.addEventListener('load', function () {
   AOS.init();
 });
+
+const carousel = document.querySelector('.carousel');
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+let currentIndex = 0;
+
+nextBtn.addEventListener('click', () => {
+  currentIndex++;
+  if (currentIndex >= carousel.children.length) {
+    currentIndex = 0;
+  }
+  updateCarousel();
+});
+
+prevBtn.addEventListener('click', () => {
+  currentIndex--;
+  if (currentIndex < 0) {
+    currentIndex = carousel.children.length - 1;
+  }
+  updateCarousel();
+});
+
+function updateCarousel() {
+  const translateX = -currentIndex * 100;
+  carousel.style.transform = `translateX(${translateX}%)`;
+}
+
+// Automatic carousel slide change
+setInterval(() => {
+  currentIndex++;
+  if (currentIndex >= carousel.children.length) {
+    currentIndex = 0;
+  }
+  updateCarousel();
+}, 5000); // Change slide every 5 seconds (adjust as needed)

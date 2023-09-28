97dae1a102a3e82f01a97a6b6d396842a6f3825e
mohit838 <mohitul449@gmail.com>
2023-09-28 10:39:54 +0600

    CENTRE FOR ARTIFICIAL

---
+++ jacindry/js/extra.js
@@ -0,0 +1,6 @@
+new BeerSlider(document.getElementById("compare"));
+new BeerSlider(document.getElementById("compare-2"));
+new BeerSlider(document.getElementById("compare-3"));
+new BeerSlider(document.getElementById("compare-4"));
+new BeerSlider(document.getElementById("compare-5"));
+new BeerSlider(document.getElementById("compare-6"));

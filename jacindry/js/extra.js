1dd4045a2745b6d0ecc1fb50b650cf46de2df777
mohit838 <mohitul449@gmail.com>
2023-09-28 11:51:52 +0600

    image gallary 50 percent

--- jacindry/js/extra.js
+++ jacindry/js/extra.js
@@ -1,6 +1,13 @@
-new BeerSlider(document.getElementById("compare"));
-new BeerSlider(document.getElementById("compare-2"));
-new BeerSlider(document.getElementById("compare-3"));
-new BeerSlider(document.getElementById("compare-4"));
-new BeerSlider(document.getElementById("compare-5"));
-new BeerSlider(document.getElementById("compare-6"));
+new BeerSlider(document.getElementById('compare'));
+new BeerSlider(document.getElementById('compare-2'));
+new BeerSlider(document.getElementById('compare-3'));
+new BeerSlider(document.getElementById('compare-4'));
+new BeerSlider(document.getElementById('compare-5'));
+new BeerSlider(document.getElementById('compare-6'));
+
+// Initialize Lightbox2 for your image gallery
+lightbox.option({
+  resizeDuration: 200,
+  wrapAround: true, // Allows navigation from last to first image and vice versa
+  positionFromTop: 100, // Adjust as needed
+});
